d127934b2355a6de2d04f4f54c5354050825e0b9
bezrukavyi <yaroslav.bezrukavyi@gmail.com>
2017-10-16 20:01:18 +0300

    Added s3

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
-role :app, %w{deployer@95.85.56.131}
-role :web, %w{deployer@95.85.56.131}
-role :db,  %w{deployer@95.85.56.131}
+role :app, %w[deployer@52.36.197.93]
+role :web, %w[deployer@52.36.197.93]
+role :db,  %w[deployer@52.36.197.93]
 
 set :branch, fetch(:branch, 'master')
 set :deploy_to, '/home/deployer/www/to_do_api'
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,23 @@
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  if Rails.env.test? || Rails.env.development?
+    config.storage = :file
+    config.enable_processing = false
+  end
+
+  if Rails.env.production?
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['S3_ACCESS_KEY'],
+      aws_secret_access_key: ENV['S3_SECRET_KEY'],
+      region: ENV['S3_REGION'],
+      endpoint: ENV['S3_ENDPOINT'],
+      path_style: true
+    }
+    config.fog_directory = ENV['S3_BUCKET']
+  end
+end
